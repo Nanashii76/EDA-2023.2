37e4813ef4590a3228e2fa266a6f175b41bb276a
Nanashi <53798700+Nanashii76@users.noreply.github.com>
2023-10-26 00:58:35 -0300

    Update 8_filaCircular.c

--- lista3/8_filaCircular.c
+++ lista3/8_filaCircular.c
@@ -9,26 +9,19 @@ typedef struct {
 
 int enfileira (fila *f, int x) {
 
-    int cap = (f->fim+1)%f->N;
-
-    if (f->v[cap] == f->v[f->inicio]) {
-        return 0;
-    } else {
-       f->v[f->fim] = x;
-       f->fim++;
-       return 1;
-    }
-
+    if ((f->fim+1)%f->N == f->inicio) 
+        return 0; 
+    f->v[f->fim++] = x;
+    
+    if (f->fim == f->N) f->fim = 0;
+        return 1;
 }
 
 int desenfileira (fila *f, int *y) {
-
-    if (f == NULL) {
+    if (f->inicio == f->fim) 
         return 0;
-    } else {
-        *y=f->v[f->inicio];
-        f->inicio = (f->inicio+1)%f->N;
+    *y= f->v[f->inicio++];
+    
+    if (f->inicio == f->N) f->inicio = 0;
         return 1;
-    }
-
 }
